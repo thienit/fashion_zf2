81fc98229830a2656a5f4ed85f791007949a0b99
thienit <banmaixanhh@gmail.com>
2014-07-11 08:41:23 +0700

    + add map

--- module/Contact/src/Contact/Controller/ContactController.php
+++ module/Contact/src/Contact/Controller/ContactController.php
@@ -6,8 +6,22 @@ use Zend\View\Model\ViewModel;
 
 class ContactController extends AbstractActionController
 {	
+	protected $subjectTable;
+	
+	public function getSubjectTable()
+	{
+		if(!$this->subjectTable){
+			$sm = $this->getServiceLocator();
+			$this->subjectTable = $sm->get('Product\Model\SubjectTable');
+		}
+		return $this->subjectTable;
+	}
+	
 	public function indexAction()
 	{
+		$subjects = $this->getSubjectTable()->fetchAll();
+		$this->layout()->subjects = $subjects;
+		
 		return new ViewModel();
 	}
 }
